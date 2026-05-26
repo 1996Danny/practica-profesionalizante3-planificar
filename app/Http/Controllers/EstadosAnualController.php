2f7ff8ee5587e72b11176903462b48d8cbc006d7
JohannaMiranda <mirandajohanna88@gmail.com>
2026-05-25 23:03:13 -0300

    Primer commit

--- app/Http/Controllers/EstadosAnualController.php
+++ app/Http/Controllers/EstadosAnualController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class Estados_anualController extends Controller
+class EstadosAnualController extends Controller
 {
     // Mostrar todos los registros
     public function index()
